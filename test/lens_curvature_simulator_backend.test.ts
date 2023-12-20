f7f951c5225a78ac6419cc967f5633d5facae1eb
ngch43 <neil.chacon@mediaadgo.com>
2023-12-20 18:14:29 +0100

    Initial commit

---
+++ test/lens_curvature_simulator_backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LensCurvatureSimulatorBackend from '../lib/lens_curvature_simulator_backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lens_curvature_simulator_backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LensCurvatureSimulatorBackend.LensCurvatureSimulatorBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
